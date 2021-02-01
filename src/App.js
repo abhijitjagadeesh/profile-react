6155573debe5f910e730a15314ac045d74a655a9
Ahijit Jagadeesh <abjagade@synamedia.com>
2021-02-01 23:23:55 +0000

    profile react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import profileImage from "./img/profile.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={profileImage} alt="profile-image" />
+      <h1>Abhijit Jagadeesh</h1>
+      <h2>Senior Software Engineer</h2>
+      <h3>Southampton, UK</h3>
     </div>
   );
 }
